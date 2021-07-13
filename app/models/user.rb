b329b3ef2f062d401a5de6a379a1a9f39318e238
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-07-13 16:32:44 +1000

    Add default role enums to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,8 @@ class User < ApplicationRecord
   has_many :project_users
   has_many :projects, through: :project_users
 
+  enum role: [:user, :system_administrator]
+  attribute :role, default: 0
+
   validates :email, presence: true, uniqueness: true
 end
