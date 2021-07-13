faf384c9bea3ccdadff5d492eaa81bc70215598d
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-07-13 14:00:23 +1000

    Fix migration error

--- db/migrate/20210713005814_create_entries.rb
+++ db/migrate/20210713005814_create_entries.rb
@@ -2,7 +2,7 @@ class CreateEntries < ActiveRecord::Migration[6.1]
   def change
     create_table :entries do |t|
       t.references :ticket, null: false, foreign_key: true
-      t.references :author, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.string :subject_string
       t.text :body
 
