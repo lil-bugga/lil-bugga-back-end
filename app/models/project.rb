8692cdec357c7049b22d4859bd3144938e573576
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-07-14 10:59:45 +1000

    Add validation to project models

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,9 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :project_users
   has_many :tickets
-  has_one :project_details
+  has_one :project_details, dependent: :destroy
+  accepts_nested_attributes_for :project_details
+
+  enum status: [:open, :closed]
+  attribute :status, default: 0
 end
--- app/models/project_detail.rb
+++ app/models/project_detail.rb
@@ -1,3 +1,6 @@
 class ProjectDetail < ApplicationRecord
   belongs_to :project
+
+  validates :project_name, presence: true, uniqueness: true
+  validates :description, default: "No description available"
 end
