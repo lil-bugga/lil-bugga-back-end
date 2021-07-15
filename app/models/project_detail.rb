05b4a6013be2bf347af66dff9823c23c805304a0
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-07-15 21:49:27 +1000

    Configure projects controller CRUD methods Includes authentication

--- app/models/project_detail.rb
+++ app/models/project_detail.rb
@@ -2,5 +2,7 @@ class ProjectDetail < ApplicationRecord
   belongs_to :project
 
   validates :project_name, presence: true, uniqueness: true
-  validates :description, default: "No description available"
+  # Set default description if none passed
+  # Note: Sending a blank string will set a blank description
+  attribute :description, default: "No description available"
 end
