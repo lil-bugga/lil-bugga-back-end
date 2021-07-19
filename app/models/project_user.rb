4f61b35fe9531fbcca6fdbfee520372727345647
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-07-19 20:21:38 +1000

    Add validation to ProjectUser model

--- app/models/project_user.rb
+++ app/models/project_user.rb
@@ -1,4 +1,15 @@
 class ProjectUser < ApplicationRecord
   belongs_to :user
   belongs_to :project
+
+  validates :user_id, presence: true
+  validates :role, presence: true
+  validates :project_id, presence: true
+
+  # Confirms the user does not already have an assigned role on the project
+  validates_uniqueness_of :user_id, scope: :project_id, message: 'User is already assigned to this project'
+
+  def self.find_user_in_project(user_id, project_id)
+    where(user_id: user_id, project_id: project_id)
+  end
 end
