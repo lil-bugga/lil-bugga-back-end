0901cf6de29e49ddf159581a1045ea87d706ca1a
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-07-20 10:19:35 +1000

    Update ProjectUser methods for better DRY implementation

--- app/models/project_user.rb
+++ app/models/project_user.rb
@@ -15,15 +15,15 @@ class ProjectUser < ApplicationRecord
   validates :role, numericality:
   {greater_than_or_equal_to: 0, less_than_or_equal_to: 3}
 
-  def self.find_user_in_project(user_id, project_id)
-    where(user_id: user_id, project_id: project_id)
+  def self.find_user_in_project(user, project)
+    where(user_id: user, project_id: project)
   end
 
   # Verify user has required permissions on project.
   # Takes a user obj or id, the project_obj or id, and the enum role (string or int)
   def self.verify_role(user_id, project, role)
     user_arr = find_user_in_project(user_id, project)
-    user_arr.each { |user| return true if user.role <= role }
+    user_arr.each { |user| return true if user.role >= role }
     false
   end
 end
