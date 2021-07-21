937e2759e633e25ba853d117cffbb7a99ae6826d
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-07-21 18:50:43 +1000

    Embed first entry and project detail in :ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -11,14 +11,36 @@ class Ticket < ApplicationRecord
   # Validates enum is within enum array
   validates :status, inclusion: { in: :status }
 
+  def self.build_ticket_object(data)
+    data.map do |item|
+      {
+        id: item.id,
+        project_id: item.project_id,
+        project_detail: ProjectDetail.find_by_id(item.project_id),
+        first_entry: item.entries.first || { error: "No entries for this ticket" },
+        user_id: item.user_id,
+        status: item.status,
+        created_at: item.created_at,
+        updated_at: item.updated_at
+       }
+    end
+  end
+
   # Method to select all tickets for project ID
   def self.all_for_project(project_id)
-    where(project_id: project_id)
+    data = where(project_id: project_id)
+    build_ticket_object(data)
   end
-  
+
   # Method to select all tickets for user id
   def self.all_for_user(user_id)
-    where(user_id: user_id)
+    data = where(user_id: user_id)
+    build_ticket_object(data)
+  end
+
+  def self.all_tickets
+    data = Ticket.all
+    build_ticket_object(data)
   end
 
 end
