355b89447ea1291ca6f02f8695b9fe9a6977de4c
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-07-24 19:12:15 +1000

    Add project detail testing

--- spec/factories/project_details.rb
+++ spec/factories/project_details.rb
@@ -2,7 +2,5 @@ FactoryBot.define do
   factory :project_detail do
     association :project
     project_name { 'test project ' }
-    description { 'This is a test project description' }
-    status {  }
   end
 end
